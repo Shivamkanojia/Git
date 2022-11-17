2db8ca69e0a91220d835bedd8df3631d03d885ea
Shivam kanojia <kanojiashivam9@gmail.com>
2022-11-17 12:52:07 +0530

    Changes in Sum,.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
-public class Sum{
+public class Sum {
     public static void main(String[] args) {
-        int a=7;
-        int b=9;
-        System.out.println(a+b);
+        int a = 7;
+        int b = 9;
+        System.out.println(a - b);
     }
 }
