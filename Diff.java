bac21e83645e1b6a91a87b4e62033ba32bc34964
Shivam kanojia <kanojiashivam9@gmail.com>
2022-11-17 17:24:31 +0530

    Adding features in Diff

--- Diff.java
+++ Diff.java
@@ -1,7 +1,10 @@
-public class Diff{
+public class Diff {
     public static void main(String[] args) {
-        int a=10;
-        int b=8;
-        System.out.println(a/b);
+        int a = 10;
+        int b = 8;
+        System.out.println(" A fter division " + a / b);
+        int c=9;
+        int d=9;
+         System.out.println("division"+c/d);
     }
 }
